0b87095ed019e6af805cd0177c83d9fcfcd2c44b
Pipemora1324 <pipemora1324@gmail.com>
2025-10-22 08:00:41 -0500

    fix: typescript types ThemeProvider

--- my-portafolio/src/app/components/ThemeProvider.tsx
+++ my-portafolio/src/app/components/ThemeProvider.tsx
@@ -1,7 +1,11 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import type { ThemeProviderProps as NextThemesProviderProps } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: any) {
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: NextThemesProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
