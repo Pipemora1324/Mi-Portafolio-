9af11f4c26b8d2bfd98ade4338fdef3af0216da1
Pipemora1324 <pipemora1324@gmail.com>
2025-11-17 17:14:14 -0500

    actualizo minavar

--- my-portafolio/src/app/components/Navbar.tsx
+++ my-portafolio/src/app/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
       <div className="container-custom">
         <div className="flex items-center justify-between h-20">
           <Link href="/" className="text-2xl font-bold gradient-text">
-            Portfolio
+            {t.navbar?.inicio === 'Home' ? 'Portfolio' : 'Portafolio'}
           </Link>
 
           {/* Desktop Navigation */}
