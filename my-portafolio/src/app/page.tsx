b852e5402d5592132dd687cb6fa6c686b1407bc0
Pipemora1324 <pipemora1324@gmail.com>
2025-11-17 15:58:53 -0500

    corrigo el page

--- my-portafolio/src/app/page.tsx
+++ my-portafolio/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Capacidades from "./components/Capacidades";
 import SobreMi from "./components/SobreMi";
@@ -7,12 +6,10 @@ import MisProyectos from "./components/MisProyectos";
 import MisServicios from "./components/MisServicios";
 import Testimonios from "./components/Testimonios";
 import HabilidadesBlandas from "./components/HabilidadesBlandas";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Navbar />
       <Hero />
       <Capacidades />
       <SobreMi />
@@ -21,7 +18,6 @@ export default function Home() {
       <MisServicios />
       <Testimonios />
       <HabilidadesBlandas />
-      <Footer />
     </main>
   );
 }
